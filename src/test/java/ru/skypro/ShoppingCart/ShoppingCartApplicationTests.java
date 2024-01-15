9f1d253a197e5d17dafce9b21e0745f9b0e44262
Александр <ignatenko.ap@yandex.ru>
2024-01-15 16:24:47 +0300

    shopping_cart_1

---
+++ src/test/java/ru/skypro/ShoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.ShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
